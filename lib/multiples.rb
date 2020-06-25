07e08882abdd00ad721afd26b65439825fbbeab3
mnavoy4 <mnavoy4@gmail.com>
2020-06-25 08:53:28 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+require 'pry'
+
+def collect_multiples(limit)
+    (1...limit).select do |number|
+        number % 3 == 0 || number % 5 == 0 
+    end
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
